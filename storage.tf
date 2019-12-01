4295bfd65673b444e6b6813c38c605e5c7ac30c6
Mark van der Laan <markkvdlaan93@gmail.com>
2019-12-01 17:53:23 +0100

    Add key

--- storage.tf
+++ storage.tf
@@ -1,7 +1,10 @@
-# resource "aws_instance" "example" {
-#     ami = "${var.aws_ami_ubuntu_server_16_04_lts}"
-#     instance_type = "t2.micro"
-#     tags {
-#         Name = "example"
-#     }
-# }
+resource "aws_instance" "example" {
+    ami = "${var.aws_ami_ubuntu_server_16_04_lts}"
+    instance_type = "${var.aws_instance_type}"
+    subnet_id = "${aws_subnet.subnet_public.id}"
+    vpc_security_group_ids = ["${aws_security_group.sg_ssh.id}"]
+    key_name = "${aws_key_pair.key_pair.key_name}"
+    tags {
+        "Environment" = "${var.aws_environment_tag}"
+    }
+}
