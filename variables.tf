a548428d0cb3dae865eb4858fcb727692f6efb2e
Mark van der Laan <markkvdlaan93@gmail.com>
2019-12-01 16:01:41 +0100

    Add variables for VPC, remove main.tf and add dedicated file for vm's

--- variables.tf
+++ variables.tf
@@ -5,3 +5,18 @@ variable "aws_profile" {}
 variable "aws_ami_ubuntu_server_16_04_lts" {
     default = "ami-0987ee37af7792903"
 }
+variable "aws_instance_type" {
+    default = "t2.micro"
+}
+variable "aws_cidr_vpc" {
+  default = "10.1.0.0/16"
+}
+variable "aws_cidr_subnet" {
+  default = "10.1.0.0/24"
+}
+variable "aws_environment_tag" {
+  default = "production"
+}
+variable "public_key_path" {
+  default = "~/.ssh/id_rsa_aws.pub"
+}
