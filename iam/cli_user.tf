1567683fe5d2adc092d04e7c96fa4ed5a26325d2
Mark van der Laan <markkvdlaan93@gmail.com>
2019-12-28 17:53:31 +0100

    Add CLI user to iam directory

---
+++ iam/cli_user.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_group_membership" "cli_user_group_membership" {
+    name = "cli_user_group_membership"
+
+    users = [
+        "${aws_iam_user.cli_user.name}",
+    ]
+
+    group = "${aws_iam_group.cli_user_group.name}"
+}
+
+resource "aws_iam_user" "cli_user" {
+    name = "cli_user"
+}
+
+resource "aws_iam_access_key" "cli_user_key" {
+    user = "${aws_iam_user.cli_user.name}"
+}
+
+resource "aws_iam_group" "cli_user_group" {
+    name = "cli_user_group"
+}
+
+resource "aws_iam_group_policy_attachment" "cli_user_ec2_full_access" {
+    group       = "${aws_iam_group.cli_user_group.name}"
+    policy_arn  = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
