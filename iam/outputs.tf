020f93e0c8ccd95240501012bc10012aebe73389
Mark van der Laan <markkvdlaan93@gmail.com>
2019-12-28 17:34:12 +0100

    Also print user in iam sub directory

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -1,3 +1,7 @@
+output "terraform_cli_user" {
+  value = "${aws_iam_access_key.terraform_end_user_key.user}"
+}
+
 output "terraform_cli_user_key" {
   value = "${aws_iam_access_key.terraform_end_user_key.secret}"
 }
