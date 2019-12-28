cb810cfede4ccd85bcf0efc247388e3d6a4493ee
Mark van der Laan <markkvdlaan93@gmail.com>
2019-12-28 17:14:47 +0100

    Add dedicated terraform project for creating iam end user policies

---
+++ iam/main.tf
@@ -0,0 +1,51 @@
+resource "aws_iam_group_membership" "terraform_end_user_group_membership" {
+    name = "terraform_end_user_group_membership"
+
+    users = [
+        "${aws_iam_user.terraform_end_user.name}",
+    ]
+
+    group = "${aws_iam_group.terraform_end_user_group.name}"
+}
+
+resource "aws_iam_user" "terraform_end_user" {
+    name = "terraform_end_user"
+}
+
+resource "aws_iam_access_key" "terraform_end_user_key" {
+  user = "${aws_iam_user.terraform_end_user.name}"
+}
+
+resource "aws_iam_group" "terraform_end_user_group" {
+    name = "terraform_end_user_group"
+}
+
+resource "aws_iam_group_policy_attachment" "ec2_full_access" {
+    group       = "${aws_iam_group.terraform_end_user_group.name}"
+    policy_arn  = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "ec2_container_registry_full_access" {
+    group       = "${aws_iam_group.terraform_end_user_group.name}"
+    policy_arn  = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "s3_full_access" {
+    group       = "${aws_iam_group.terraform_end_user_group.name}"
+    policy_arn  = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "kinesis_full_access" {
+    group       = "${aws_iam_group.terraform_end_user_group.name}"
+    policy_arn  = "arn:aws:iam::aws:policy/AmazonKinesisFullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "dynamo_db_full_access" {
+    group       = "${aws_iam_group.terraform_end_user_group.name}"
+    policy_arn  = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "sns_full_access" {
+    group       = "${aws_iam_group.terraform_end_user_group.name}"
+    policy_arn  = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
---
+++ iam/outputs.tf
@@ -0,0 +1,3 @@
+# output "terraform_cli_user_key" {
+#   value = "${aws_iam_access_key.terraform_end_user_key.encrypted_secret}"
+# }
