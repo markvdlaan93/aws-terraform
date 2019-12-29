96959511c56dd47fb6fa5c7a0c2f66bf7cd0fdd0
Mark van der Laan <markkvdlaan93@gmail.com>
2019-12-29 16:17:52 +0100

    Reference to new variable names inside providers.tf

--- iam/providers.tf
+++ iam/providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key  = "${var.aws_ec2_access_key}"
-    secret_key  = "${var.aws_ec2_secret_key}"
+    access_key  = "${var.aws_ec2_access_key_iam_user}"
+    secret_key  = "${var.aws_ec2_secret_key_iam_user}"
     region      = "${var.aws_region}"
     profile     = "${var.aws_profile}"
     version     = "~> 2.0"
